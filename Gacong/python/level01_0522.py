18763bf34efa0f157e23af62137ead30a0833cee
choigabin <63983505+choigabin@users.noreply.github.com>
2021-05-22 21:18:52 +0900

    0522

--- Gacong/python/level01_0522.py
+++ Gacong/python/level01_0522.py
@@ -11,7 +11,7 @@
 # 모든 원소에 대해 앞서 설명한 연산을 적용했을 때 나온 결과를 배열에 담아 return 하도록 solution 함수를 작성해주세요.
 
 def solution(array, commands):
-    array[array[0]:array[1]].sort
+    array[array[0]:array[1]].sort()
     answer = []
     return answer
 
